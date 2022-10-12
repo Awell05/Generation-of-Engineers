3bf5487df3f04d6ad949bb149335eb7b28e59067
Novo Persona <adcaldwell93@yahoo.com>
2022-10-12 10:54:33 -0400

    added classes and subclasses to team js files

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,8 +2,8 @@ const manager = require('../lib/Manager');
 
 describe('manager',() =>{
     it ('should generate a prompt for the user to input managers name, employee ID, email address, and office number'), () => {
-        // const managerInfo = {prompt.name, prompt.id, prompt.email,prompt.officeNumber};
+        // const manager = new Manager {prompt.name, prompt.id, prompt.email,prompt.officeNumber};
         
-        expect(manager).toEqual()
+        expect(manager).toEqual(managerInfo);
     }
 })
