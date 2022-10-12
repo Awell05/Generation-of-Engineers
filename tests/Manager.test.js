f822261e080ec4682953f2c1ab94d761630b66ee
Novo Persona <adcaldwell93@yahoo.com>
2022-10-12 01:02:19 -0400

    started on managertest js

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,9 @@
 const manager = require('../lib/Manager');
+
+describe('manager',() =>{
+    it ('should generate a prompt for the user to input managers name, employee ID, email address, and office number'), () => {
+        // const managerInfo = {prompt.name, prompt.id, prompt.email,prompt.officeNumber};
+        
+        expect(manager).toEqual()
+    }
+})
