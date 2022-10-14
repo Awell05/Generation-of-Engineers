d3da82a93bc239b94ba4dfca63e0d82b8e2bfd5e
Novo Persona <adcaldwell93@yahoo.com>
2022-10-13 22:37:17 -0400

    manger.test.js test pass

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,8 +1,12 @@
 const {Manager} = require('../lib/Manager');
 const managerInfo = new Manager ("Leroy", 2, "leroy@email.com", 5687898741);
 describe('Manager',() =>{
-    it ('it should create an new manager class', () => {
+    it ('should create an new manager class', () => {
         expect(typeof(managerInfo)).toEqual("object");
     })
 })
-
+describe('class Manager role property',() =>{
+    it('the getRole method should return role', () => {
+        expect(managerInfo.getRole()).toEqual('Manager');
+    } )
+})
